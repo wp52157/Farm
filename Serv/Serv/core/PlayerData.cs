bb7ab5d097ffbee5cc17acf3d5ba537698951e76
Mz <1484754907@qq.com>
2019-01-02 20:37:13 +0800

    wp

---
+++ Serv/Serv/core/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+[Serializable]
+public class PlayerData
+{
+    public int sj = 0;
+    public int mff = 0;
+    public int mkktd = 0;
+    public int pttd = 0;
+    public int jtd = 0;
+    public int zstd = 0;
+
+    public PlayerData()
+    {
+        sj = 12000;
+    }
+}
---
+++ Serv/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegExApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "sdhedhiuhdh(&&(&(&Y(*&(*(*&111";
+            Regex reg = new Regex("^[A-Za-z0-9]+$");
+            MatchCollection result = reg.Matches(str);
+            foreach (var item in result)
+            {
+                Console.Write(item.ToString());
+            }
+        }
+    }
+}
